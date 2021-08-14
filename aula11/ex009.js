f15faea8582740e5d27768df870983f598353b1a
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-08-14 18:41:06 -0300

    Aula 011 - Ex009 e EX010 - CursoJS

---
+++ aula11/ex009.js
@@ -0,0 +1,19 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60){
+    console.log("")
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log("")
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+console.log("-----------------------------------------")
+console.log("")
+
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log('Você é Estrangeiro!')
+}else{
+    console.log('Você é Brasileiro!')
+}
