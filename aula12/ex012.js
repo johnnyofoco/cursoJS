eb3c3243142fc0563916039a1c77d9fc70980281
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-08-16 19:56:36 -0300

    Aula 012 - Ex012 - CursoJS

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -27,3 +27,40 @@ if (hora <= 5) {
 }
 
 console.log('-------------------------------------------')
+
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch (diaSem) {
+    case 0:
+        console.log(`Hoje é: Domingo`)
+        break;
+
+    case 1:
+        console.log(`Hoje é: Segunda-Feira`)
+        break;
+
+    case 2:
+        console.log(`Hoje é: Terça-Feira`)
+        break;
+
+    case 3:
+        console.log(`Hoje é: Quarta-Feira`)
+        break;
+
+    case 4:
+        console.log(`Hoje é: Quinta-Feira`)
+        break;
+
+    case 5:
+        console.log(`Hoje é: Sexta-Feira`)
+        break;
+
+    case 6:
+        console.log(`Hoje é: Sabádo`)
+        break;
+
+    default:
+        console.log('[ERRO] Dia inválido!')
+        break;
+}
