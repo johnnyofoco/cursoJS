a0d147bfa4be9ca1ba0f3c74b507620f637e7c8a
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-08-17 19:51:28 -0300

    Exercícios 014 e 015 - CursoJS

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+//Para rodar no console de depuração (ctrl+f5)
+// Ou F8 para rodar via Node.js
+
+//While
+var c = 1
+
+while (c <= 3) {
+    console.log(`Tudo bem ? : ${c}`)
+    c++ // c = c + 1
+}
+
+//Do While
+c = 1
+
+do {
+    console.log(`E ae? : ${c}`)
+    c++ // c = c + 1
+} while (c <= 3)
+
+
+//For
+
+for (let index = 1; index < 11; index++) {
+    console.log(`${index}`);
+
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+
+console.log('--------------------')
+console.log('While:')
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+console.log('--------------------')
+console.log('Do While:')
+
+c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
