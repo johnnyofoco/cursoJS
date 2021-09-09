57179ed7e73569f8b8e355321221f503556f913f
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-09-09 20:54:59 -0300

    Auala sobre map()

--- javaScriptFuncional/functional/map.js
+++ javaScriptFuncional/functional/map.js
@@ -1,8 +1,20 @@
 const numbers = [1, 2, 3, 4, 5, 6]
 
-const numbersV2 = numbers.map(el => el * 2)
+const numbersV2 = numbers.map(el => el * 2) //recebe outra funcão como parâmetro
+    //PRINCIPAIS UTILIDADES:
+    /*
+     * Transformar todos os elementos do array
+     * Exemplo: dobrar os números, arredondar números...
+     * Pegar um array de objetos completos e retornar apenas um atributo
+     */
 
 console.log(numbers, numbersV2)
+
+let numbersV3 = numbers.map(function(el) {
+    return el * 2
+})
+
+console.log(numbersV3)
 
 const students = [
     { name: 'Jake', score: 6.4 },
