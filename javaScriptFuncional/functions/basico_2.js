b9ea86de29e59b58c740da7bf73f059f5544e8ad
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-09-13 19:21:34 -0300

    Aula: Passar uma função como parâmetro e uma função que retorne outra função

--- javaScriptFuncional/functions/basico_2.js
+++ javaScriptFuncional/functions/basico_2.js
@@ -6,7 +6,7 @@ const boaTarde = function() {
     console.log('Boa Tarde!!!')
 }
 
-//Passar uma função como parâmetro para outra função
+//1) Passar uma função como parâmetro para outra função
 function executarQualquerCoisa(fn) {
     if (typeof fn == 'function') {
         fn()
@@ -18,3 +18,16 @@ function executarQualquerCoisa(fn) {
 executarQualquerCoisa(3)
 executarQualquerCoisa(bomDia)
 executarQualquerCoisa(boaTarde)
+
+// 2) Retornar uma função a a partir de uma outra função
+function potencia(base) {
+    return function(exp) {
+        return Math.pow(base, exp)
+    }
+}
+
+const potenciaDe2 = potencia(2)
+console.log(potenciaDe2(8))
+
+const resultadoPotencia = potencia(3)(4)
+console.log(resultadoPotencia)
