a96c0922a18f26655946f4cfc7e38edcbe4e56b4
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-09-13 19:48:05 -0300

    Desafio 1 e Desafio2 - Funções que retornam funções e funções que recebem funções como parâmetro

---
+++ javaScriptFuncional/functions/desafio_1.js
@@ -0,0 +1,49 @@
+//############### DESAFIO 1 ##################
+// somar(3)(4)(5)
+function somar(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c
+        }
+    }
+}
+
+const resultado = somar(3)(4)(5)
+console.log(`Resultado do desafio 1: ${resultado}`)
+
+//############### DESAFIO 2 ##################
+function multiplicacao(a = 0, b = 0) {
+    return a * b
+}
+
+function soma(a = 0, b = 0) {
+    return a + b
+}
+
+function subtracao(a = 0, b = 0) {
+    return a - b
+}
+
+
+// calcular(3)(7)(fn)
+function calcular(a) {
+    return function(b) {
+        return function(fn) {
+            return fn(a, b)
+        }
+    }
+}
+
+let resultado2 = 0
+
+//fn -> 3 * 7
+resultado2 = calcular(3)(7)(multiplicacao)
+console.log(`Resultado do desafio 2 (fn -> 3 * 7): ${resultado2}`)
+
+//fn -> 3 + 7
+resultado2 = calcular(3)(7)(soma)
+console.log(`Resultado do desafio 2 (fn -> 3 + 7): ${resultado2}`)
+
+//fn -> 3 - 7
+resultado2 = calcular(3)(7)(subtracao)
+console.log(`Resultado do desafio 2 (fn -> 3 - 7): ${resultado2}`)
