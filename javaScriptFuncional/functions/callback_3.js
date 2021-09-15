c9d76bb17c04ead8f0a9bf2988aaa97e7f553dc9
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-09-15 15:26:51 -0300

    Aula Callback 3

--- javaScriptFuncional/functions/callback_3.js
+++ javaScriptFuncional/functions/callback_3.js
@@ -1,3 +1,11 @@
 const nums = [1, 2, 3, 4, 5]
-const dobro = (n, i) => n * 2 + i
-console.log(nums.map(dobro))git
+const dobro = n => n * 2
+console.log(nums.map(dobro))
+
+//const dobro = (n, i, a) => n * 2 + i + a.length
+//console.log(nums.map(dobro))
+
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
+const primeiraLetra = texto => texto[0]
+const letras = nomes.map(primeiraLetra)
+console.log(nomes, letras)
