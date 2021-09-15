977610d7130e7b16b24cc98adb3ee8d4a572d363
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-09-15 13:38:47 -0300

    Aula callback 1

--- javaScriptFuncional/functions/callback_1.js
+++ javaScriptFuncional/functions/callback_1.js
@@ -1,9 +1,19 @@
-function exec(fn, a = 0, b = 0) {
+function exec(fn, a, b) {
     fn(a, b)
 }
 const somarNoTerminal = (x, y) => console.log(x + y)
-
 const subtrairNoTerminal = (w, z) => console.log(w - z)
+const subtrair = (e, f) => e - f
 
 exec(somarNoTerminal, 56, 38)
-exec(subtrairNoTerminal, 56, 38)
+exec(subtrairNoTerminal, 182, 27)
+
+const r = exec(subtrair, 50, 25)
+console.log(r)
+
+
+//passar uma função como parâmetro para outra função
+//e quando acontece um evento a função é chamada
+
+const fn = () => console.log('Exec...')
+setInterval(fn, 1000)
