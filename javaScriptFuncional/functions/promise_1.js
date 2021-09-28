7a48dce306c2cf308e0ab7e889e3ff657f7ee5ba
johnnyofoco <johnny.marlon.paula@gmail.com>
2021-09-27 21:44:40 -0300

    Aula sobre promise

--- javaScriptFuncional/functions/promise_1.js
+++ javaScriptFuncional/functions/promise_1.js
@@ -1,8 +1,17 @@
 
-let p = new Promise(function (cumprirPromessa) {
-    cumprirPromessa(3)    
-})
+//Arrow functions:
+const primeiroElemento = arrayOuString => arrayOuString[0]
+const letraMinuscula = letra => letra.toLowerCase()
 
-p.then(function (valorPrometido) {
-    console.log(valorPrometido)
+
+//A partir do operador new um objeto do tipo Promisse foi criado.
+//Uma promisse recebe uma função
+let p = new Promise(function (resolve) {
+  resolve(['Ana', 'Pedro', 'João'])
 })
+//o método then recebe um uníco parâmetro e deve ser do tipo function
+//o método then é utilizado para receber o valor gerado pela Promise
+    .then(primeiroElemento) 
+    .then(primeiroElemento) 
+    .then(letraMinuscula)
+    .then(console.log)
